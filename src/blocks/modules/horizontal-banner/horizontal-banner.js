58073b838ca20e4d452c12d1e46c0c4e1c0c3dba
denisraimer <to.mars.66.miles@gmail.com>
2020-05-01 20:03:59 +0300

    📦 Added horizontal banner, current news

---
+++ src/blocks/modules/horizontal-banner/horizontal-banner.js
@@ -0,0 +1,13 @@
+modules.define('horizontal-banner', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
