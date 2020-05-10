b9224b16b4fb2c2ce053d7ac8f34fcc191693efb
DenisRaymer <to.mars.66.miles@gmail.com>
2020-05-11 01:34:17 +0300

    📦 Added an average news card

---
+++ src/blocks/modules/average-card-news/average-card-news.js
@@ -0,0 +1,13 @@
+modules.define('average-card-news', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
