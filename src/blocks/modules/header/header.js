6fdda65d546768bd8354f6e30c4a9aca36ac0087
DenisRaymer <to.mars.66.miles@gmail.com>
2020-10-12 00:51:23 +0300

    👌 Improved the message function
    Now you can bind a modal window to any element using the js-error-message class

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,12 +1,18 @@
 import Swal from 'sweetalert2/dist/sweetalert2.all.min';
 
-const btnRegistration = document.querySelector('.error-message');
+const btnErrorMessage = document.querySelectorAll('.js-error-message');
 
-btnRegistration.onclick = function () {
-    Swal.fire({
-        title: 'Error!',
-        text: 'This functionality is not yet available',
-        icon: 'error',
-        confirmButtonText: 'Close'
-    }).then(r => 'Error');
-};
+const errorModal = function (data) {
+    for (const element of data) {
+        element.addEventListener('click', function () {
+            Swal.fire({
+                title: 'Error!',
+                text: 'This functionality is not yet available',
+                icon: 'error',
+                confirmButtonText: 'Close'
+            }).then(r => 'Error');
+        })
+    }
+}
+
+errorModal(btnErrorMessage);
